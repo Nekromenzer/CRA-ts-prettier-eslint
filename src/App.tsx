4a6ab7e113c2ee5cac9de2622e4edd5e55d366b5
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2024-04-10 17:30:22 +0530

    basic folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Input from 'components/Input'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Input />
         <p className="mx-5 bg-red-400 pt-4 text-3xl font-bold text-red-500 hover:text-slate-400">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
