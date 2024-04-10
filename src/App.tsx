a6953591ff9b77b8fdf5866a120903e158a7ddb1
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2024-04-10 15:11:36 +0530

    presstier tailwind plugin

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="mx-5 bg-red-400 pt-4 text-3xl font-bold text-red-500 hover:text-slate-400">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
